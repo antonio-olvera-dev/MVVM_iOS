45757a26ac6b3224737877dbbc06ac2dea40a495
Antonio Olvera <antonioolvera1995@gmail.com>
2021-12-14 15:53:04 +0100

    Initial Commit

---
+++ MVVM_iOS/MVVM_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_iOSApp.swift
+//  MVVM_iOS
+//
+//  Created by Antonio Olvera Fernández on 14/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

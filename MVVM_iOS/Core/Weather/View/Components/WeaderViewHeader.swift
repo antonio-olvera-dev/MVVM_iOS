4a2aeb0f7fffe58ea3b5bfb9fe04160e2a24c7c9
Antonio Olvera <antonioolvera1995@gmail.com>
2021-12-17 19:01:15 +0100

    extract temperature and description to WeatherViewHeader

---
+++ MVVM_iOS/Core/Weather/View/Components/WeaderViewHeader.swift
@@ -0,0 +1,72 @@
+//
+//  WeaderViewHeader.swift
+//  MVVM_iOS
+//
+//  Created by Antonio Olvera Fernández on 17/12/21.
+//
+
+import SwiftUI
+
+struct WeatherViewHeader:View{
+    
+    
+    @StateObject var weatherViewModel:WeatherViewModel
+    @Binding var city:String
+    
+    
+    var body: some View {
+        VStack{
+            description()
+            temperature()
+            
+        }
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    func description() -> some View {
+        return VStack{
+            Text(weatherViewModel.weatherResponseDataModel?.city ?? city)
+                .foregroundColor(.white)
+                .font(.system(size: 70))
+            Text(weatherViewModel.weatherResponseDataModel?.weather.first?.description ?? "No temperature")
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding(.bottom, 8)
+        }
+    }
+    
+    
+    
+    func temperature() -> some View {
+        return VStack{
+            HStack {
+                if let iconURL = weatherViewModel.weatherResponseDataModel?.weather.first?.iconURLString,
+                   let url = URL(string: "http://openweathermap.org/img/wn/\(iconURL)@2x.png") {
+                    AsyncImage(url: url) { image in
+                        image
+                    } placeholder: {
+                        ProgressView()
+                    }
+                }
+                Text("\(weatherViewModel.weatherResponseDataModel?.temperature.currentTemperature ?? 0.0)º")
+                    .font(.system(size: 40))
+                    .foregroundColor(.white)
+            }
+            .padding(.top, -20)
+            HStack(spacing: 14) {
+                Label("\(weatherViewModel.weatherResponseDataModel?.temperature.maxTemperature ?? 0.0)º máx.", systemImage: "thermometer.sun.fill")
+                    .symbolRenderingMode(.multicolor)
+                    .foregroundColor(.white)
+                Label("\(weatherViewModel.weatherResponseDataModel?.temperature.minTemperature ?? 0.0)º min.", systemImage: "thermometer.snowflake")
+                    .symbolRenderingMode(.multicolor)
+                    .foregroundColor(.white)
+            }
+        }
+    }
+}
--- MVVM_iOS/Core/Weather/View/WeatherView.swift
+++ MVVM_iOS/Core/Weather/View/WeatherView.swift
@@ -13,6 +13,7 @@ struct WeatherView: View {
     @StateObject var weatherViewModel = WeatherViewModel()
     @State var city = "Madrid"
     
+    
     var body: some View {
         
         NavigationView{
@@ -28,8 +29,8 @@ struct WeatherView: View {
     
     func bodyView() -> some View{
         return VStack{
-            description()
-            temperature()
+            
+            WeatherViewHeader(weatherViewModel: weatherViewModel, city: $city)
             Divider().padding()
             humidity()
             Divider().padding()
@@ -42,47 +43,6 @@ struct WeatherView: View {
         .background( bgBodyView())
     }
     
-    
-    func description() -> some View {
-        return VStack{
-            Text(weatherViewModel.weatherResponseDataModel?.city ?? city)
-                .foregroundColor(.white)
-                .font(.system(size: 70))
-            Text(weatherViewModel.weatherResponseDataModel?.weather.first?.description ?? "No temperature")
-                .font(.headline)
-                .foregroundColor(.white)
-                .padding(.bottom, 8)
-        }
-    }
-    
-    
-    
-    func temperature() -> some View {
-        return VStack{
-            HStack {
-                if let iconURL = weatherViewModel.weatherResponseDataModel?.weather.first?.iconURLString,
-                   let url = URL(string: "http://openweathermap.org/img/wn/\(iconURL)@2x.png") {
-                    AsyncImage(url: url) { image in
-                        image
-                    } placeholder: {
-                        ProgressView()
-                    }
-                }
-                Text("\(weatherViewModel.weatherResponseDataModel?.temperature.currentTemperature ?? 0.0)º")
-                    .font(.system(size: 40))
-                    .foregroundColor(.white)
-            }
-            .padding(.top, -20)
-            HStack(spacing: 14) {
-                Label("\(weatherViewModel.weatherResponseDataModel?.temperature.maxTemperature ?? 0.0)º máx.", systemImage: "thermometer.sun.fill")
-                    .symbolRenderingMode(.multicolor)
-                    .foregroundColor(.white)
-                Label("\(weatherViewModel.weatherResponseDataModel?.temperature.minTemperature ?? 0.0)º min.", systemImage: "thermometer.snowflake")
-                    .symbolRenderingMode(.multicolor)
-                    .foregroundColor(.white)
-            }
-        }
-    }
     
     func humidity() -> some View{
         return VStack{
